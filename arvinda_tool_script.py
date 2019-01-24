0da8ad0bc387aa2edd0d3cde74e3416a66135a0e
vindard <vinda.rd@gmail.com>
2019-01-23 23:54:15 -0400

    make keys list a tuple, add json printing

--- arvinda_tool_script.py
+++ arvinda_tool_script.py
@@ -1,4 +1,5 @@
 from lightning_address import decode as decodepayreq
+import json
 
 pay_req = [
             'lnbc8800u1pwysh8kpp5kln5lcht4la2aqg899qj4gyfnglfs3wg83wmjzlh7ak682t4suvqdqqcqzys26g2z8dtjzk3vl8eh22rq32kn7lvgrl554ggmma7mwr9e5wzgjfjk8rv0md493e8ua944pchq2supmglcpt6nk5cxs5ycsmplwgkawsqk2w5pa',
@@ -26,7 +27,7 @@ test = ['lnbc8m1pwy085frzjqvxr7xwhgt9zjjj4cqphdvan2hpujrtpc6mt8925m0r6cxd3g8q57z
 keys = []
 for i in pay_req:
     public_key = decodepayreq(i)["Signed with public key"].decode('utf-8')
-    keys.append(public_key)
+    keys.append((i, public_key))
     print(f"\n")
 
-print(keys)
+print(json.dumps(keys, indent=2))
